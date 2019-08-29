463da45227a8683c219c7d427901f8d078f886cc
harjoth <harjoth.khara@gmail.com>
2019-08-29 15:40:16 -0700

    moving state data from users component to app.js and passing data down to users as props. installed axios so we can use it in CDM(life cycle method) to bring in api data. http requests and updating state.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false //if loading show a spinner, if false, show data.
+  }
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const res = await axios.get('https://api.github.com/users');
+
+    this.setState({ users: res.data, loading: false });
+  }
+
+
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
