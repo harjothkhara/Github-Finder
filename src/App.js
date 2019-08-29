839ce73fa0c6c49a145de47a1850ebd17e473d41
harjoth <harjoth.khara@gmail.com>
2019-08-29 14:15:31 -0700

    props, deafult props, proptypes(type check for correct data types) - failed prop type error will show in console.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
 import './App.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 class App extends Component {
   render() {
+    const numbers = [1,2,3,4]
     return (
       <div className="App">
-        <h1>Hello from React</h1>
+        <Navbar title={numbers}/>
       </div>
     );
   }
