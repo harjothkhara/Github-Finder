232c8f10dd95442b475a3beacd74c73dc5753df2
harjoth <harjoth.khara@gmail.com>
2019-08-29 20:29:10 -0700

    button to clear users once a user search has been done. only want the clear button to show if there is actual users

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -6,7 +6,9 @@ class Search extends Component {
   };
 
   static propTypes = {
-    searchUsers: PropTypes.func.isRequired
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired
   };
 
   onSubmit = e => {
@@ -20,6 +22,7 @@ class Search extends Component {
   };
 
   render() {
+    const { showClear, clearUsers } = this.props;
     return (
       <div>
         <form onSubmit={this.onSubmit} className="form">
@@ -36,6 +39,12 @@ class Search extends Component {
             className="btn btn-dark btn-block"
             />
         </form>
+        {/* if true show the button */}
+        {showClear && (
+          <button
+            className="btn btn-light btn-block"
+            onClick={clearUsers}>Clear</button>
+        )}
       </div>
     )
   }
