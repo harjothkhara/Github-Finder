5e7b7d32cc57e37f71d36c454478c833d12426df
harjoth <harjoth.khara@gmail.com>
2019-08-31 13:20:55 -0700

    user component UI and layout...added link that takes you back to the search page from the user page w/o losing the users in from the page(still in state)..styled profile page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment, Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import User from './components/users/User';
 import Search from './components/users/Search';
 import axios from 'axios';
 import Alert from './components/layout/Alert';
@@ -12,6 +13,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false, //if loading show a spinner, if false, show data.
     alert: null
   }
@@ -29,10 +31,25 @@ class App extends Component {
     this.setState({ loading: true });
 
     const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+      `https://api.github.com/search/users?q=${text}&client_id=$
+        {process.env.REACT_APP_GITHUB_CLIENT_ID}
+        &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
     this.setState({ users: res.data.items, loading: false });
   };
+
+  // Get a single Github user
+  getUser = async (username) => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID}
+        &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({ user: res.data, loading: false });
+  }
+
 
   // Clear users from state
   clearUsers = () => this.setState({ users: [], loading: false });
@@ -45,7 +62,7 @@ class App extends Component {
   };
 
   render() {
-    const { users, loading } = this.state;
+    const { users, user, loading } = this.state;
 
     return (
       <Router>
@@ -70,6 +87,9 @@ class App extends Component {
             )}
            />
            <Route exact path='/about' component={About} />
+           <Route exact path='/user/:login' render={props => (
+             <User {...props} getUser={this.getUser} user={user} loading={loading} />
+           )} />
           </Switch>
         </div>
       </div>
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types'; //impt
+import { Link } from 'react-router-dom';
 
 const UserItem = ({ user: { login, avatar_url, html_url } }) => { //destructuring user prop needed up here and pulling from prop what we need
     return (
@@ -9,9 +10,9 @@ const UserItem = ({ user: { login, avatar_url, html_url } }) => { //destructurin
         <h3>{login}</h3>
 
         <div>
-          <a href={html_url} className="btn btn-dark btn-sm my-1">
+          <Link to={`/user/${login}`} className="btn btn-dark btn-sm my-1">
             More
-          </a>
+          </Link>
         </div>
       </div>
     );
